48de13b6af64bdfbc8ea70089292ffd57f6baa22
Rubens Maroti F <rubens@mobilemed.com.br>
2022-07-19 14:19:03 -0300

    Fix request metadata

--- src/AwsFargateMetadata.js
+++ src/AwsFargateMetadata.js
@@ -13,123 +13,68 @@ export default class AwsFargateMetadata {
     constructor(config = {}) {
         this.logger = config.logger || new Logger();
         this.host = process.env.ECS_CONTAINER_METADATA_URI_V4;
-        this.metadata = null;
-        this.requestMetadata();
     }
 
     fetchMetadata(resultsCallback) {
-        async.parallel({
-            'ami-id': callback => {
-                this.lookupMetadataKey('ImageID', callback);
-            },
-            'instance-id': callback => {
-                this.lookupMetadataKey('DockerId', callback);
-            },
-            'instance-type': callback => {
-                callback(null, 'FARGATE');
-            },
-            'local-ipv4': callback => {
-                this.lookupNetworkMetadata('IPv4Addresses', callback);
-            },
-            'local-hostname': callback => {
-                this.lookupNetworkMetadata('PrivateDNSName', callback);
-            },
-            'availability-zone': callback => {
-                this.lookupMetadataKey('AvailabilityZone', callback);
-            },
-            'public-hostname': callback => {
-                // Same as local-hostname, because until now AWS doesn't provide public DNS name in the endpoint
-                this.lookupNetworkMetadata('PrivateDNSName', callback);
-            },
-            'public-ipv4': callback => {
-                // Same as local-ipv4, because until now AWS doesn't provide public IPv4 in the endpoint
-                this.lookupNetworkMetadata('IPv4Addresses', callback);
-            },
-            'mac': callback => {
-                this.lookupNetworkMetadata('MACAddress', callback);
-            },
-            'vpc-id': callback => {
-                // awsvpc (fixed, because until now AWS doesn't provide VPC ID in the endpoint)
-                callback(null, 'awsvpc');
-            },
-            accountId: callback => {
-                // the accountId is in the identity document.
-                this.lookupInstanceIdentity(callback);
-            }
-        }, (error, results) => {
-            this.logger.debug('Found Instance AWS Metadata', results);
-            const filteredResults = Object.keys(results).reduce((filtered, prop) => {
-                if (results[prop]) filtered[prop] = results[prop];
-                return filtered;
-            }, {});
-            resultsCallback(filteredResults);
-        });
-    }
-
-    requestMetadata() {
         request.get({
             url: this.host,
         }, (error, response, body) => {
             if (error || response.statusCode !== 200) {
                 this.logger.error('Error requesting metadata', error);
+                resultsCallback(null);
             } else {
-                this.metadata = body;
+                this.logger.info('Received metadata', body);
+                const metadata = JSON.parse(body);
+                const results = {
+                    'ami-id': this.lookupMetadataKey('ImageID', metadata),
+                    'instance-id': this.lookupMetadataKey('DockerId', metadata),
+                    'instance-type': 'FARGATE',
+                    'local-ipv4': this.lookupNetworkMetadata('IPv4Addresses', metadata),
+                    'local-hostname': this.lookupNetworkMetadata('PrivateDNSName', metadata),
+                    'availability-zone': this.lookupMetadataKey('AvailabilityZone', metadata),
+                    'public-hostname': this.lookupNetworkMetadata('PrivateDNSName', metadata),
+                    'public-ipv4': this.lookupNetworkMetadata('IPv4Addresses', metadata),
+                    'mac': this.lookupNetworkMetadata('MACAddress', metadata),
+                    'vpc-id': 'awsvpc',
+                    'accountId': this.lookupInstanceIdentity(metadata)
+                }
+                this.logger.debug('Parsed Instance AWS Metadata', results);
+                const filteredResults = Object.keys(results).reduce((filtered, prop) => {
+                    if (results[prop]) filtered[prop] = results[prop];
+                    return filtered;
+                }, {});
+                resultsCallback(filteredResults);
             }
         });
     }
 
-    lookupMetadataKey(key, callback) {
-        if (!this.metadata) {
-            callback(null, null);
-            return;
-        }
-
+    lookupMetadataKey(key, metadata) {
         if (this.metadata[key] === null || this.metadata[key] === undefined) {
-            callback(null, null);
-            return;
+            return null;
         }
 
-        callback(null, this.metadata[key]);
+        return metadata[key];
     }
 
-    lookupNetworkMetadata(key, callback) {
-        if (!this.metadata) {
-            callback(null, null);
-            return;
-        }
-
-        if (this.metadata.Networks && this.metadata.Networks.length > 0) {
-            const property = this.metadata.Networks[0][key];
+    lookupNetworkMetadata(key, metadata) {
+        if (metadata.Networks && metadata.Networks.length > 0) {
+            const property = metadata.Networks[0][key];
             if (!property) {
-                callback(null, null);
-                return
+                return null;
             }
-            callback(null, Array.isArray(property) ? property[0] : property);
+            return Array.isArray(property) ? property[0] : property;
         }
-
-        if (this.metadata[key] === null || this.metadata[key] === undefined) {
-            callback(null, null);
-            return;
-        }
-
-        callback(null, this.metadata[key]);
     }
 
-    lookupInstanceIdentity(callback) {
-        if (!this.metadata) {
-            callback(null, null);
-            return;
-        }
-
-        if (this.metadata.ContainerARN) {
+    lookupInstanceIdentity(metadata) {
+        if (metadata.ContainerARN) {
             // ContainerARN.split(':')[4] (arn:aws:ecs:us-west-2:111122223333)
             const arn = this.metadata.ContainerARN.split(':');
             if (arn.length === 5) {
-                callback(null, arn[4]);
-                return;
+                return arn[4];
             }
         }
 
-        callback(null, null);
+        return null;
     }
 }
